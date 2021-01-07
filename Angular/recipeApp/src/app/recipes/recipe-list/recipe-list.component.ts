6b6821079312eb858c92714ea2945dbf18903770
SuhyeonBae <suhyeon.b01@gmail.com>
2021-01-07 17:04:26 +0900

    Add recipe detail and shopping list

--- Angular/recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes : Recipe[] = [
     new Recipe('Salad', 'Salad Test Description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/beetroot_halloumi_salad-e828cae.jpg?quality=90&resize=500%2C454'),
+    new Recipe('Curry', 'Curry Test Description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/113777-0b21d44.jpg?quality=90&resize=960,872'),
   ];
 
   constructor() { }
