38db37126d66f87a91f951cf7ccd3061fa993f0e
SuhyeonBae <suhyeon.b01@gmail.com>
2021-01-07 13:38:11 +0900

    Add recipe list section

--- Angular/recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/recipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes : Recipe[] = [
+    new Recipe('Salad', 'Salad Test Description', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/beetroot_halloumi_salad-e828cae.jpg?quality=90&resize=500%2C454'),
+  ];
 
   constructor() { }
 
